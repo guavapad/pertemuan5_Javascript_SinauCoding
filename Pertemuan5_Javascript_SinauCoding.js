b626f057488a82df07e044d8fab8ec85f07ce648
guavapad <perdanafasluki@gmail.com>
2022-10-02 22:34:57 +0700

    tugas javascript

---
+++ Pertemuan5_Javascript_SinauCoding.js
@@ -0,0 +1,9 @@
+for(let i=0;i<=10;i++){
+    if(i==8){
+        console.log(`bus transjakarta ${i} sedang lembur`)
+    }else if(i>6){
+        console.log(`bus transjakarta ${i} sedang tidak beroperasi`)
+    }else{
+        console.log(`bus transjakarta ${i} beroperasi dengan baik`)
+    }
+}
